2bc39b40748c9bba87b388d049c77ae938e04424
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-11-27 20:35:22 -0500

    chore: remove unneeded services

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import authRouter from './auth/auth.routes';
 import healthRouter from './health/health.routes';
-import interactionsRouter from './interactions/interactions.routes';
 
 const appRouter = express.Router();
 
 appRouter.use('/auth', authRouter);
 appRouter.use('/health', healthRouter);
-appRouter.use('/interactions', interactionsRouter);
 
 export default appRouter;
