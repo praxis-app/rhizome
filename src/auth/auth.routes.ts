9debe0663664dba4cc30d8c4bc0c64eca3c8c9eb
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-12-03 00:24:52 -0500

    chire: remove console logs

--- src/auth/auth.routes.ts
+++ src/auth/auth.routes.ts
@@ -6,7 +6,6 @@ export const authRouter = express.Router();
 authRouter.use(authService.authenticateUser);
 
 authRouter.post('/', async (_, res) => {
-  console.log('POST /auth');
   const token = await authService.register(res);
   res.json({ token });
 });
