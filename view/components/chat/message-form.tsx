a529e3bbeba97859a2f069e92c2dfb845f76572a
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-12-09 02:00:47 -0500

    feat: add message form layout

--- view/components/chat/message-form.tsx
+++ view/components/chat/message-form.tsx
@@ -1,22 +1,18 @@
 import { Send } from '@mui/icons-material';
 import { Box, FormGroup, IconButton, Input, SxProps } from '@mui/material';
 import { t } from 'i18next';
-import { useAboveBreakpoint } from '../../hooks/shared.hooks';
 import AttachedImagePreview from '../images/attached-image-preview';
 import ImageInput from '../images/image-input';
+import { grey } from '@mui/material/colors';
 
 const MessageForm = () => {
-  const isAboveLarge = useAboveBreakpoint('lg');
-
   const formStyles: SxProps = {
-    position: isAboveLarge ? undefined : 'fixed',
-    bottom: isAboveLarge ? undefined : '65px',
-    left: isAboveLarge ? undefined : 0,
+    position: 'fixed',
+    bottom: '0px',
+    left: 0,
     bgcolor: 'background.paper',
-    paddingY: isAboveLarge ? 0.6 : 1,
-    paddingX: isAboveLarge ? 0.5 : 0.9,
-    borderRadius: isAboveLarge ? 4 : 0,
-    maxWidth: isAboveLarge ? undefined : '100%',
+    paddingY: 1,
+    paddingX: 0.9,
     width: '100%',
   };
   const inputStyles: SxProps = {
@@ -31,41 +27,39 @@ const MessageForm = () => {
   };
 
   return (
-    <>
-      <Box sx={formStyles}>
-        <FormGroup row>
-          <Box
-            bgcolor="background.secondary"
-            borderRadius={4}
-            paddingX={1.5}
-            paddingY={0.2}
-            flex={1}
-          >
-            <Input
-              autoComplete="off"
-              placeholder={t('chat.prompts.sendAMessage')}
-              sx={inputStyles}
-              value={''}
-              disableUnderline
-              multiline
+    <Box sx={formStyles}>
+      <FormGroup row>
+        <Box
+          bgcolor={grey[900]}
+          borderRadius={4}
+          paddingX={1.5}
+          paddingY={0.2}
+          flex={1}
+        >
+          <Input
+            autoComplete="off"
+            placeholder={t('chat.prompts.sendAMessage')}
+            sx={inputStyles}
+            value={''}
+            disableUnderline
+            multiline
+          />
+
+          <Box display="flex" justifyContent="space-between">
+            <ImageInput
+              iconStyles={{ color: 'text.secondary', fontSize: 25 }}
+              multiple
             />
 
-            <Box display="flex" justifyContent="space-between">
-              <ImageInput
-                iconStyles={{ color: 'text.secondary', fontSize: 25 }}
-                multiple
-              />
-
-              <IconButton sx={sendButtonStyles} edge="end" disableRipple>
-                <Send sx={{ fontSize: 20, color: 'text.secondary' }} />
-              </IconButton>
-            </Box>
+            <IconButton sx={sendButtonStyles} edge="end" disableRipple>
+              <Send sx={{ fontSize: 20, color: 'text.secondary' }} />
+            </IconButton>
           </Box>
-        </FormGroup>
+        </Box>
+      </FormGroup>
 
-        <AttachedImagePreview selectedImages={[]} sx={{ marginLeft: 1.5 }} />
-      </Box>
-    </>
+      <AttachedImagePreview selectedImages={[]} sx={{ marginLeft: 1.5 }} />
+    </Box>
   );
 };
 
