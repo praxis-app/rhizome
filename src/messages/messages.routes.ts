79221b63f1cca11d640eaa2418c57d27ae0e705e
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-12-08 19:45:40 -0500

    feat: protect message routes

--- src/messages/messages.routes.ts
+++ src/messages/messages.routes.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { messagesController } from './messages.controller';
+import { authService } from '../auth/auth.service';
 
 export const messagesRouter = express.Router();
 
+messagesRouter.use(authService.authenticateUser);
 messagesRouter.get('/', messagesController.getMessages);
