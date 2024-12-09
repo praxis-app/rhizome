00b9f4955b14e1ddb2bd9e16f4480d4b6449c5f3
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-12-08 19:08:47 -0500

    feat: fetch channels on home page

--- view/pages/home-page.tsx
+++ view/pages/home-page.tsx
@@ -1,3 +1,27 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import ProgressBar from '../components/shared/progress-bar';
+import { useAppStore } from '../store/app.store';
+
 export const HomePage = () => {
-  return <>TODO: Show channels and messages here</>;
+  const token = useAppStore((state) => state.token);
+
+  const { data, isLoading } = useQuery({
+    queryKey: 'health',
+    queryFn: async () => {
+      const result = await axios.get('/api/channels');
+      return result.data;
+    },
+    enabled: !!token,
+  });
+
+  if (isLoading) {
+    return <ProgressBar />;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  return <>{JSON.stringify(data)}</>;
 };
