0c1e9d565548b393016f752d1feccb16e2b4d3ba
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-12-17 20:18:53 -0500

    refactor: remove unneeded middleware for message router

--- src/messages/messages.router.ts
+++ src/messages/messages.router.ts
@@ -1,14 +1,11 @@
 import express from 'express';
-import { authService } from '../auth/auth.service';
+import { uploadImages } from '../images/middleware/upload-images.middleware';
 import { messagesController } from './messages.controller';
 
 export const messagesRouter = express.Router({
   mergeParams: true,
 });
 
-// TODO: Determine if this is needed
-messagesRouter.use(authService.authenticateUser);
-
 messagesRouter.get('/', messagesController.getMessages);
 messagesRouter.post('/', messagesController.createMessage);
-messagesRouter.post('/:messageId/images', messagesController.createMessageImages);
+messagesRouter.post('/:messageId/images', uploadImages, messagesController.createMessageImages);
