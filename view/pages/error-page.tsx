1d7dbcc566d4915398dffdda55b2451c45004ddb
forrestwilkins <ethanforrestwilkins@gmail.com>
2024-12-23 14:01:57 -0500

    feat: update error page

--- view/pages/error-page.tsx
+++ view/pages/error-page.tsx
@@ -1,12 +1,37 @@
-import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import errorGif from '../assets/images/error.gif';
+import LazyLoadImage from '../components/images/lazy-load-image';
+import { useAboveBreakpoint } from '../hooks/shared.hooks';
 import { Layout } from '../components/app/layout';
 
-export const ErrorPage = () => {
+const ErrorPageContent = () => {
+  const isLarge = useAboveBreakpoint('md');
+  const { t } = useTranslation();
+
   return (
-    <Layout>
-      <Typography fontSize={22} marginTop={3} marginLeft={2.5}>
-        Something went wrong.
-      </Typography>
-    </Layout>
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <LazyLoadImage
+        src={errorGif}
+        alt={t('errors.somethingWentWrong')}
+        width={isLarge ? '40%' : '80%'}
+        marginBottom={isLarge ? 0 : 15}
+        borderRadius="9999px"
+        alignSelf="center"
+        display="block"
+      />
+    </Box>
   );
 };
+
+export const ErrorPage = () => (
+  <Layout>
+    <ErrorPageContent />
+  </Layout>
+);
