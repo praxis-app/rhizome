1408f520077c90f297d18963ef06bb21f625ed1c
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-01-02 13:54:23 -0500

    refactor: rm unneeded auth wrapper

--- view/components/app/layout.tsx
+++ view/components/app/layout.tsx
@@ -1,28 +1,60 @@
-import { Container, CssBaseline, SxProps, ThemeProvider } from '@mui/material';
-import { ReactNode } from 'react';
+import {
+  Container,
+  CssBaseline,
+  LinearProgress,
+  linearProgressClasses,
+  LinearProgressProps,
+  styled,
+  SxProps,
+  ThemeProvider,
+} from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+import { useMeQuery } from '../../hooks/user.hooks';
+import { useAppStore } from '../../store/app.store';
 import { theme } from '../../styles/theme';
-import { AuthWrapper } from '../auth/auth-wrapper';
 import Toast from '../shared/toast';
+
+const ProgressBar = styled(LinearProgress)<LinearProgressProps>(
+  ({ theme }) => ({
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+      ...theme.applyStyles('dark', {
+        backgroundColor: '#09090b',
+      }),
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+      ...theme.applyStyles('dark', {
+        backgroundColor: '#18181b',
+      }),
+    },
+    height: '100vh',
+  }),
+);
+
+const queryClient = new QueryClient();
 
 interface Props {
   children: ReactNode;
   sx?: SxProps;
 }
 
-const queryClient = new QueryClient();
-
 export const Layout = ({ children, sx }: Props) => {
+  const { isAppLoading } = useAppStore((state) => state);
+
+  useMeQuery({ retry: false });
+
+  if (isAppLoading) {
+    return <ProgressBar />;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme} defaultMode="system">
         <CssBaseline />
-        <AuthWrapper>
-          <Container maxWidth="sm" sx={sx}>
-            {children}
-            <Toast />
-          </Container>
-        </AuthWrapper>
+        <Container maxWidth="sm" sx={sx}>
+          {children}
+          <Toast />
+        </Container>
       </ThemeProvider>
     </QueryClientProvider>
   );
