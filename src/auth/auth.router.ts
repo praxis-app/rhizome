918e5e63e037df9cd0bd39461e870805d07c82e3
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-01-05 10:07:36 -0500

    refactor: clean up auth router

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,15 +1,10 @@
 import express from 'express';
-import * as authController from './auth.controller';
+import { createAnonSession, logOut, signUp, upgradeAnonSession } from './auth.controller';
 import * as authService from './auth.service';
 
 export const authRouter = express.Router();
 
-authRouter.post('/', authService.validateSignUp, authController.signUp);
-authRouter.post('/anon', authController.createAnonSession);
-authRouter.post('/logout', authController.logOut);
-authRouter.put(
-  '/anon',
-  authService.authenticate,
-  authService.validateSignUp,
-  authController.upgradeAnonSession,
-);
+authRouter.post('/anon', createAnonSession);
+authRouter.put('/anon', authService.authenticate, authService.validateSignUp, upgradeAnonSession);
+authRouter.post('/', authService.validateSignUp, signUp);
+authRouter.post('/logout', logOut);
