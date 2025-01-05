f55b448c81e59f1d8867da063c474966e254d7e7
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-01-04 19:04:37 -0500

    refactor: convert to classes to fns for health module

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,11 +1,7 @@
 import { Request, Response } from 'express';
-import { healthService } from './health.service';
+import * as healthService from './health.service';
 
-class HealthController {
-  async getHealth(_: Request, res: Response) {
-    const payload = healthService.getHealth();
-    res.json(payload);
-  }
-}
-
-export const healthController = new HealthController();
+export const getHealth = async (_: Request, res: Response) => {
+  const payload = healthService.getHealth();
+  res.json(payload);
+};
