316bf53c0803c9e501a052d0c6b83dcb6b262a33
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-01-11 16:25:34 -0500

    feat: add action and subject fields for Permission entity

--- src/roles/permission.entity.ts
+++ src/roles/permission.entity.ts
@@ -8,10 +8,29 @@ import {
 } from 'typeorm';
 import { Role } from './role.entity';
 
+export enum PermissionAction {
+  CREATE = 'create',
+  READ = 'read',
+  UPDATE = 'update',
+  DELETE = 'delete',
+}
+
+export enum PermissionSubject {
+  CHANNEL = 'channel',
+  MESSAGE = 'message',
+  ROLE = 'role',
+}
+
 @Entity()
 export class Permission {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'enum', enum: PermissionAction })
+  action: PermissionAction;
+
+  @Column({ type: 'enum', enum: PermissionSubject })
+  subject: PermissionSubject;
 
   @ManyToOne(() => Role, (role) => role.permissions, {
     onDelete: 'CASCADE',
