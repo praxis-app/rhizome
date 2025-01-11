c57bc1b27f6c57229c750a0775e27a30d60a33a3
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-01-11 11:37:52 -0500

    feat: use gray token

--- view/styles/theme.ts
+++ view/styles/theme.ts
@@ -123,7 +123,7 @@ export const theme = createTheme({
           ...theme.applyStyles('light', {
             boxShadow:
               '0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px -1px rgba(0, 0, 0, .1);',
-            border: '1px solid rgb(228, 228, 231)',
+            border: `1px solid ${GRAY[100]}`,
           }),
         }),
       },
