8e8829b8ec1067ab24ff45a6b24d9b4ac9e11a33
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-01-11 20:59:58 -0500

    feat: add getUserPermisions fn

--- src/roles/create-ability.util.ts
+++ src/roles/create-ability.util.ts
@@ -1,14 +1,12 @@
 import { createMongoAbility, ForcedSubject, MongoAbility, RawRuleOf } from '@casl/ability';
 import { AbilityAction, AbilitySubject } from './models/role.types';
 
-type Abilities = [AbilityAction, AbilitySubject | ForcedSubject<Exclude<AbilitySubject, 'all'>>];
+export type Abilities = [
+  AbilityAction,
+  AbilitySubject | ForcedSubject<Exclude<AbilitySubject, 'all'>>,
+];
 
-type AppAbility = MongoAbility<Abilities>;
-
-// TODO: Uncomment when no longer needed
-// const testRules: RawRuleOf<AppAbility>[] = [
-//   { action: ['read', 'create'], subject: 'Channel' },
-// ];
+export type AppAbility = MongoAbility<Abilities>;
 
 export const createAbility = (rules: RawRuleOf<AppAbility>[]) =>
   createMongoAbility<AppAbility>(rules);
---
+++ src/roles/roles.service.ts
@@ -0,0 +1,42 @@
+import { RawRuleOf } from '@casl/ability';
+import { dataSource } from '../database/data-source';
+import { AppAbility } from './create-ability.util';
+import { Role } from './models/role.entity';
+import { AbilityAction, AbilitySubject } from './models/role.types';
+
+type PermissionsMap = Record<string, AbilityAction[]>;
+
+const roleRepository = dataSource.getRepository(Role);
+
+// TODO: Uncomment when no longer needed
+// const testRules: RawRuleOf<AppAbility>[] = [
+//   { action: ['read', 'create'], subject: 'Channel' },
+// ];
+
+export const getUserPermisions = async (userId: string): Promise<RawRuleOf<AppAbility>[]> => {
+  const roles = await roleRepository.find({
+    relations: ['permissions'],
+    where: {
+      members: {
+        id: userId,
+      },
+    },
+  });
+
+  const permissionsMap = roles.reduce<PermissionsMap>((result, role) => {
+    for (const permission of role.permissions) {
+      if (!result[permission.subject]) {
+        result[permission.subject] = [];
+      }
+      result[permission.subject].push(permission.action);
+    }
+    return result;
+  }, {});
+
+  const permissions = Object.entries(permissionsMap).map(([subject, actions]) => ({
+    subject: subject as AbilitySubject,
+    action: actions,
+  }));
+
+  return permissions;
+};
