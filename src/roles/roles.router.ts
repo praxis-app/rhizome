5cf97c76c25e829496fd1a7a0d5c1e1cdb247f40
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-01-20 15:44:58 -0500

    feat: use can middleware in roles router

--- src/roles/roles.router.ts
+++ src/roles/roles.router.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { authenticate } from '../auth/middleware/authenticate.middleware';
+import { can } from './middleware/can.middleware';
 import {
   addRoleMembers,
   createRole,
@@ -16,13 +17,25 @@ export const rolesRouter = express.Router();
 
 rolesRouter.use(authenticate);
 
-rolesRouter.get('/:roleId', getRole);
-rolesRouter.get('/', getRoles);
-rolesRouter.post('/', createRole);
-rolesRouter.put('/:roleId', updateRole);
-rolesRouter.delete('/:roleId', deleteRole);
+rolesRouter.get('/:roleId', can(['read'], 'Role'), getRole);
+rolesRouter.get('/', can(['read'], 'Role'), getRoles);
+rolesRouter.post('/', can(['create'], 'Role'), createRole);
+rolesRouter.put('/:roleId', can(['update'], 'Role'), updateRole);
+rolesRouter.delete('/:roleId', can(['delete'], 'Role'), deleteRole);
 
-rolesRouter.post('/:roleId/members', addRoleMembers);
-rolesRouter.delete('/:roleId/members/:userId', deleteRoleMember);
-rolesRouter.get('/:roleId/members/eligible', getUsersEligibleForRole);
-rolesRouter.put('/:roleId/permissions', updateRolePermissions);
+rolesRouter.post('/:roleId/members', can(['create'], 'Role'), addRoleMembers);
+rolesRouter.delete(
+  '/:roleId/members/:userId',
+  can(['read'], 'Role'),
+  deleteRoleMember,
+);
+rolesRouter.get(
+  '/:roleId/members/eligible',
+  can(['read'], 'Role'),
+  getUsersEligibleForRole,
+);
+rolesRouter.put(
+  '/:roleId/permissions',
+  can(['read'], 'Role'),
+  updateRolePermissions,
+);
