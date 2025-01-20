febd86447dfc73148f01de9f67c24f8bfee68ef9
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-01-20 16:16:24 -0500

    refactor: add member and perm routers

---
+++ src/roles/role-members.router.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { can } from './middleware/can.middleware';
+import * as rolesController from './roles.controller';
+
+export const roleMembersRouter = express.Router();
+
+roleMembersRouter.post(
+  '/',
+  can(['update'], 'Role'),
+  rolesController.addRoleMembers,
+);
+
+roleMembersRouter.delete(
+  '/:userId',
+  can(['update'], 'Role'),
+  rolesController.deleteRoleMember,
+);
+
+roleMembersRouter.get(
+  '/eligible',
+  can(['read'], 'Role'),
+  rolesController.getUsersEligibleForRole,
+);
--- src/roles/roles.router.ts
+++ src/roles/roles.router.ts
@@ -1,16 +1,14 @@
 import express from 'express';
 import { authenticate } from '../auth/middleware/authenticate.middleware';
 import { can } from './middleware/can.middleware';
+import { roleMembersRouter } from './role-members.router';
+import { rolePermissionsRouter } from './role-permissions.router';
 import {
-  addRoleMembers,
   createRole,
   deleteRole,
-  deleteRoleMember,
   getRole,
   getRoles,
-  getUsersEligibleForRole,
   updateRole,
-  updateRolePermissions,
 } from './roles.controller';
 
 export const rolesRouter = express.Router();
@@ -23,19 +21,5 @@ rolesRouter.post('/', can(['create'], 'Role'), createRole);
 rolesRouter.put('/:roleId', can(['update'], 'Role'), updateRole);
 rolesRouter.delete('/:roleId', can(['delete'], 'Role'), deleteRole);
 
-rolesRouter.post('/:roleId/members', can(['create'], 'Role'), addRoleMembers);
-rolesRouter.delete(
-  '/:roleId/members/:userId',
-  can(['read'], 'Role'),
-  deleteRoleMember,
-);
-rolesRouter.get(
-  '/:roleId/members/eligible',
-  can(['read'], 'Role'),
-  getUsersEligibleForRole,
-);
-rolesRouter.put(
-  '/:roleId/permissions',
-  can(['read'], 'Role'),
-  updateRolePermissions,
-);
+rolesRouter.use('/:roleId/permissions', rolePermissionsRouter);
+rolesRouter.use('/:roleId/members', roleMembersRouter);
