2e1c2e4a646c4bbe7292918cf0fe6a5f52fcffac
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-02-08 14:48:25 -0500

    feat: account for special chars in validateChannel

--- src/channels/middleware/validate-channel.middleware.ts
+++ src/channels/middleware/validate-channel.middleware.ts
@@ -3,6 +3,7 @@ import { sanitizeText } from '../../common/common.utils';
 import { CreateChannelReq } from '../channels.service';
 
 const CHANNEL_NAME_MAX = 25;
+const VALID_NAME_REGEX = /^[A-Za-z0-9 ]+$/;
 
 export const validateChannel = (
   req: Request,
@@ -18,6 +19,10 @@ export const validateChannel = (
   if (sanitizedName.length > CHANNEL_NAME_MAX) {
     const message = `Channel name cannot exceed ${CHANNEL_NAME_MAX} characters`;
     res.status(422).send(message);
+    return;
+  }
+  if (!VALID_NAME_REGEX.test(sanitizedName)) {
+    res.status(422).send('Channel names cannot contain special characters');
     return;
   }
   next();
