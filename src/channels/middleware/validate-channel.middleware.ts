ed82eacdd54114aea38661a0b218bfd9f86b6160
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-02-09 13:07:25 -0500

    feat: add validation for channel description

--- src/channels/middleware/validate-channel.middleware.ts
+++ src/channels/middleware/validate-channel.middleware.ts
@@ -6,18 +6,21 @@ import { CreateChannelReq } from '../channels.service';
 const VALID_CHANNEL_NAME_REGEX = /^[A-Za-z0-9-]+$/;
 
 const CHANNEL_NAME_MAX = 25;
+const CHANNEL_DESCRIPTION_MAX = 2000;
 
 export const validateChannel = (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const channel = req.body as CreateChannelReq;
-  if (typeof channel.name !== 'string') {
+  const { name, description } = req.body as CreateChannelReq;
+  if (typeof name !== 'string') {
     res.status(422).send('Channel name must be a string');
     return;
   }
-  const sanitizedName = sanitizeText(channel.name);
+
+  // Channel name validation
+  const sanitizedName = sanitizeText(name);
   if (sanitizedName.length > CHANNEL_NAME_MAX) {
     const message = `Channel name cannot exceed ${CHANNEL_NAME_MAX} characters`;
     res.status(422).send(message);
@@ -28,5 +31,18 @@ export const validateChannel = (
     res.status(422).send(message);
     return;
   }
+
+  // Channel description validation
+  const sanitizedDescription = sanitizeText(description);
+  if (description && typeof description !== 'string') {
+    res.status(422).send('Channel description must be a string');
+    return;
+  }
+  if (description && sanitizedDescription.length > CHANNEL_DESCRIPTION_MAX) {
+    const message = `Channel description cannot exceed ${CHANNEL_DESCRIPTION_MAX} characters`;
+    res.status(422).send(message);
+    return;
+  }
+
   next();
 };
