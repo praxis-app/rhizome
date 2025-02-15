7aa273b7239105c1ba7dc3c738a5ccb3a98ea60b
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-02-15 16:22:57 -0500

    refactor: add ChannelView

---
+++ view/components/channels/channel-view.tsx
@@ -0,0 +1,157 @@
+import { Box, debounce } from '@mui/material';
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
+import { useRef } from 'react';
+import { useParams } from 'react-router-dom';
+import { api } from '../../client/api-client';
+import ChannelTopNav from '../../components/channels/channel-top-nav';
+import MessageFeed from '../../components/messages/message-feed';
+import MessageForm from '../../components/messages/message-form';
+import LeftNav from '../../components/nav/left-nav/left-nav';
+import { useAboveBreakpoint, useSubscription } from '../../hooks/shared.hooks';
+import { useMeQuery } from '../../hooks/user.hooks';
+import { useAppStore } from '../../store/app.store';
+import { Channel } from '../../types/channel.types';
+import { Message, MessagesQuery } from '../../types/message.types';
+import { PubSubMessage } from '../../types/shared.types';
+import ChannelSkeleton from './channel-skeleton';
+
+enum MessageType {
+  MESSAGE = 'message',
+  IMAGE = 'image',
+}
+
+interface NewMessagePayload {
+  type: MessageType.MESSAGE;
+  message: Message;
+}
+
+interface ImageMessagePayload {
+  type: MessageType.IMAGE;
+  isPlaceholder: false;
+  messageId: string;
+  imageId: string;
+}
+
+interface Props {
+  channel: Channel;
+}
+
+const ChannelView = ({ channel }: Props) => {
+  const { isLoggedIn } = useAppStore((state) => state);
+
+  const { channelId } = useParams();
+  const queryClient = useQueryClient();
+  const feedBoxRef = useRef<HTMLDivElement>(null);
+  const isAboveMd = useAboveBreakpoint('md');
+
+  const { data: meData } = useMeQuery({
+    enabled: isLoggedIn,
+  });
+
+  const { data: messagesData, fetchNextPage } = useInfiniteQuery({
+    queryKey: ['messages', channelId],
+    queryFn: ({ pageParam }) => {
+      return api.getChannelMessages(channelId!, pageParam);
+    },
+    getNextPageParam: (_lastPage, pages) => {
+      return pages.flatMap((page) => page.messages).length;
+    },
+    initialPageParam: 0,
+    enabled: !!channelId,
+  });
+
+  useSubscription(`channel-${channelId}-${meData?.user.id}`, {
+    onMessage: (event) => {
+      const { body }: PubSubMessage<NewMessagePayload | ImageMessagePayload> =
+        JSON.parse(event.data);
+      if (!body) {
+        return;
+      }
+
+      // Update cache with new message, images are placeholders
+      if (body.type === MessageType.MESSAGE) {
+        queryClient.setQueryData<MessagesQuery>(
+          ['messages', channelId],
+          (oldData) => {
+            if (!oldData) {
+              return {
+                pages: [{ messages: [body.message] }],
+                pageParams: [0],
+              };
+            }
+            const pages = oldData.pages.map((page, index) => {
+              if (index === 0) {
+                return {
+                  messages: [body.message, ...page.messages],
+                };
+              }
+              return page;
+            });
+            return { pages, pageParams: oldData.pageParams };
+          },
+        );
+      }
+
+      // Update cache with image status once uploaded
+      if (body.type === MessageType.IMAGE) {
+        queryClient.setQueryData<MessagesQuery>(
+          ['messages', channelId],
+          (oldData) => {
+            if (!oldData) {
+              return { pages: [], pageParams: [] };
+            }
+
+            const pages = oldData.pages.map((page) => {
+              const messages = page.messages.map((message) => {
+                if (message.id !== body.messageId || !message.images) {
+                  return message;
+                }
+                const images = message.images.map((image) => {
+                  if (image.id !== body.imageId) {
+                    return image;
+                  }
+                  return { ...image, isPlaceholder: false };
+                });
+                return { ...message, images };
+              });
+              return { messages };
+            });
+
+            return { pages, pageParams: oldData.pageParams };
+          },
+        );
+      }
+
+      scrollToBottom();
+    },
+    enabled: !!meData && !!channelId,
+  });
+
+  const scrollToBottom = () => {
+    if (feedBoxRef.current && feedBoxRef.current.scrollTop >= -200) {
+      feedBoxRef.current.scrollTop = 0;
+    }
+  };
+
+  if (!messagesData) {
+    return <ChannelSkeleton />;
+  }
+
+  return (
+    <Box display="flex" position="fixed" top={0} left={0} bottom={0} right={0}>
+      {isAboveMd && <LeftNav me={meData?.user} />}
+
+      <Box display="flex" flexDirection="column" flex={1}>
+        <ChannelTopNav channel={channel} />
+        <MessageFeed
+          feedBoxRef={feedBoxRef}
+          onLoadMore={debounce(fetchNextPage, 500)}
+          messages={messagesData.pages.flatMap((page) => page.messages)}
+        />
+        <MessageForm channelId={channel.id} onSend={scrollToBottom} />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelView;
--- view/pages/channels/channel-page.tsx
+++ view/pages/channels/channel-page.tsx
@@ -1,51 +1,14 @@
-import { Box, debounce } from '@mui/material';
-import {
-  useInfiniteQuery,
-  useQuery,
-  useQueryClient,
-} from '@tanstack/react-query';
-import { useRef } from 'react';
+import { Typography } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import { api } from '../../client/api-client';
 import ChannelSkeleton from '../../components/channels/channel-skeleton';
-import ChannelTopNav from '../../components/channels/channel-top-nav';
-import MessageFeed from '../../components/messages/message-feed';
-import MessageForm from '../../components/messages/message-form';
-import LeftNav from '../../components/nav/left-nav/left-nav';
-import { useAboveBreakpoint, useSubscription } from '../../hooks/shared.hooks';
-import { useMeQuery } from '../../hooks/user.hooks';
-import { useAppStore } from '../../store/app.store';
-import { Message, MessagesQuery } from '../../types/message.types';
-import { PubSubMessage } from '../../types/shared.types';
-
-enum MessageType {
-  MESSAGE = 'message',
-  IMAGE = 'image',
-}
-
-interface NewMessagePayload {
-  type: MessageType.MESSAGE;
-  message: Message;
-}
-
-interface ImageMessagePayload {
-  type: MessageType.IMAGE;
-  isPlaceholder: false;
-  messageId: string;
-  imageId: string;
-}
+import ChannelView from '../../components/channels/channel-view';
 
 export const ChannelPage = () => {
-  const { isLoggedIn } = useAppStore((state) => state);
-
   const { channelId } = useParams();
-  const queryClient = useQueryClient();
-  const feedBoxRef = useRef<HTMLDivElement>(null);
-  const isAboveMd = useAboveBreakpoint('md');
-
-  const { data: meData } = useMeQuery({
-    enabled: isLoggedIn,
-  });
+  const { t } = useTranslation();
 
   const { data: channelData, isLoading: isChannelLoading } = useQuery({
     queryKey: ['channels', channelId],
@@ -53,115 +16,13 @@ export const ChannelPage = () => {
     enabled: !!channelId,
   });
 
-  const { data: messagesData, fetchNextPage } = useInfiniteQuery({
-    queryKey: ['messages', channelId],
-    queryFn: ({ pageParam }) => {
-      return api.getChannelMessages(channelId!, pageParam);
-    },
-    getNextPageParam: (_lastPage, pages) => {
-      return pages.flatMap((page) => page.messages).length;
-    },
-    initialPageParam: 0,
-    enabled: !!channelId,
-  });
-
-  useSubscription(`channel-${channelId}-${meData?.user.id}`, {
-    onMessage: (event) => {
-      const { body }: PubSubMessage<NewMessagePayload | ImageMessagePayload> =
-        JSON.parse(event.data);
-      if (!body) {
-        return;
-      }
-
-      // Update cache with new message, images are placeholders
-      if (body.type === MessageType.MESSAGE) {
-        queryClient.setQueryData<MessagesQuery>(
-          ['messages', channelId],
-          (oldData) => {
-            if (!oldData) {
-              return {
-                pages: [{ messages: [body.message] }],
-                pageParams: [0],
-              };
-            }
-            const pages = oldData.pages.map((page, index) => {
-              if (index === 0) {
-                return {
-                  messages: [body.message, ...page.messages],
-                };
-              }
-              return page;
-            });
-            return { pages, pageParams: oldData.pageParams };
-          },
-        );
-      }
-
-      // Update cache with image status once uploaded
-      if (body.type === MessageType.IMAGE) {
-        queryClient.setQueryData<MessagesQuery>(
-          ['messages', channelId],
-          (oldData) => {
-            if (!oldData) {
-              return { pages: [], pageParams: [] };
-            }
-
-            const pages = oldData.pages.map((page) => {
-              const messages = page.messages.map((message) => {
-                if (message.id !== body.messageId || !message.images) {
-                  return message;
-                }
-                const images = message.images.map((image) => {
-                  if (image.id !== body.imageId) {
-                    return image;
-                  }
-                  return { ...image, isPlaceholder: false };
-                });
-                return { ...message, images };
-              });
-              return { messages };
-            });
-
-            return { pages, pageParams: oldData.pageParams };
-          },
-        );
-      }
-
-      scrollToBottom();
-    },
-    enabled: !!meData && !!channelId,
-  });
-
-  const scrollToBottom = () => {
-    if (feedBoxRef.current && feedBoxRef.current.scrollTop >= -200) {
-      feedBoxRef.current.scrollTop = 0;
-    }
-  };
-
   if (isChannelLoading) {
     return <ChannelSkeleton />;
   }
 
-  if (!channelData || !messagesData) {
-    return null;
+  if (!channelData) {
+    return <Typography>{t('errors.somethingWentWrong')}</Typography>;
   }
 
-  return (
-    <Box display="flex" position="fixed" top={0} left={0} bottom={0} right={0}>
-      {isAboveMd && <LeftNav me={meData?.user} />}
-
-      <Box display="flex" flexDirection="column" flex={1}>
-        <ChannelTopNav channel={channelData.channel} />
-        <MessageFeed
-          feedBoxRef={feedBoxRef}
-          onLoadMore={debounce(fetchNextPage, 500)}
-          messages={messagesData.pages.flatMap((page) => page.messages)}
-        />
-        <MessageForm
-          channelId={channelData.channel.id}
-          onSend={scrollToBottom}
-        />
-      </Box>
-    </Box>
-  );
+  return <ChannelView channel={channelData.channel} />;
 };
