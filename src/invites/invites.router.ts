8ed150830bef3b22aebc15de9ae581ff588b981f
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-02-17 21:07:37 -0500

    feat: add invitesRouter

---
+++ src/invites/invites.router.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import {
+  createInvite,
+  deleteInvite,
+  getInvite,
+  getInvites,
+} from './invites.controller';
+import { authenticate } from '../auth/middleware/authenticate.middleware';
+
+export const invitesRouter = express.Router();
+
+invitesRouter
+  .get('/', getInvites)
+  .get('/:inviteId', authenticate, getInvite)
+  .post('/', authenticate, createInvite)
+  .delete('/:inviteId', authenticate, deleteInvite);
