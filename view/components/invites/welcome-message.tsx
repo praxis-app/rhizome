4aa39d946d8a2deb8b426c8584f7f4aeff4c151a
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-02-23 17:06:37 -0500

    feat: show sign up button in welcome message for first user

--- view/components/invites/welcome-message.tsx
+++ view/components/invites/welcome-message.tsx
@@ -1,12 +1,25 @@
 import { Button, Typography } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import { api } from '../../client/api-client';
 import { NavigationPaths } from '../../constants/shared.constants';
 import { useAppStore } from '../../store/app.store';
 import BotMessage from '../messages/bot-message';
 
 const WelcomeMessage = () => {
   const { inviteToken } = useAppStore((state) => state);
+
+  const { data } = useQuery({
+    queryKey: ['is-first-user'],
+    queryFn: api.isFirstUser,
+  });
+
+  const signUpPath = data?.isFirstUser
+    ? NavigationPaths.SignUp
+    : `${NavigationPaths.SignUp}/${inviteToken}`;
+
+  const showSignUp = data?.isFirstUser || !!inviteToken;
 
   const { t } = useTranslation();
   const navigate = useNavigate();
@@ -29,10 +42,10 @@ const WelcomeMessage = () => {
         {t('welcome.messages.inDev')}
       </Typography>
 
-      {!!inviteToken && (
+      {showSignUp && (
         <Button
           variant="contained"
-          onClick={() => navigate(`${NavigationPaths.SignUp}/${inviteToken}`)}
+          onClick={() => navigate(signUpPath)}
           sx={{ textTransform: 'uppercase', marginRight: '12px' }}
         >
           {t('users.actions.signUp')}
