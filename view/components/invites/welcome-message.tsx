2fa099df90e2ac9a679280be60e106741487e880
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-02-22 22:00:31 -0500

    feat: show sign up and login buttons in welcome message

--- view/components/invites/welcome-message.tsx
+++ view/components/invites/welcome-message.tsx
@@ -1,9 +1,15 @@
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { NavigationPaths } from '../../constants/shared.constants';
+import { useAppStore } from '../../store/app.store';
 import BotMessage from '../messages/bot-message';
 
 const WelcomeMessage = () => {
+  const { inviteToken } = useAppStore((state) => state);
+
   const { t } = useTranslation();
+  const navigate = useNavigate();
 
   return (
     <BotMessage bodySx={{ paddingTop: 0.9 }}>
@@ -19,7 +25,27 @@ const WelcomeMessage = () => {
         {t('welcome.messages.projectDescription2')}
       </Typography>
 
-      <Typography>{t('welcome.messages.inDev')}</Typography>
+      <Typography sx={{ marginBottom: 1.5 }}>
+        {t('welcome.messages.inDev')}
+      </Typography>
+
+      {!!inviteToken && (
+        <Button
+          variant="contained"
+          onClick={() => navigate(NavigationPaths.SignUp)}
+          sx={{ textTransform: 'uppercase', marginRight: '12px' }}
+        >
+          {t('users.actions.signUp')}
+        </Button>
+      )}
+
+      <Button
+        variant="contained"
+        onClick={() => navigate(NavigationPaths.Login)}
+        sx={{ textTransform: 'uppercase' }}
+      >
+        {t('users.actions.logIn')}
+      </Button>
     </BotMessage>
   );
 };
