b9928afc5fd5cae07e2e8888e89e96e788d7ef09
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-03-01 11:05:44 -0500

    feat: add perm checks for invite routes

--- src/invites/invites.router.ts
+++ src/invites/invites.router.ts
@@ -6,11 +6,14 @@ import {
   getInvite,
   getInvites,
 } from './invites.controller';
+import { can } from '../roles/middleware/can.middleware';
 
 export const invitesRouter = express.Router();
 
+invitesRouter.get('/:token', getInvite);
+
 invitesRouter
-  .get('/:token', getInvite)
-  .get('/', authenticate, getInvites)
-  .post('/', authenticate, createInvite)
-  .delete('/:inviteId', authenticate, deleteInvite);
+  .use(authenticate)
+  .get('/', can('read', 'Invite'), getInvites)
+  .post('/', can('create', 'Invite'), createInvite)
+  .delete('/:inviteId', can('delete', 'Invite'), deleteInvite);
