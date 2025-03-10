e17af8708990088b6963f359c04a6ac8c383a97f
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-03-09 23:36:08 -0400

    feat: add votes service

--- src/proposals/proposals.service.ts
+++ src/proposals/proposals.service.ts
@@ -3,6 +3,13 @@ import { sanitizeText } from '../common/common.utils';
 import { dataSource } from '../database/data-source';
 import { deleteImageFile } from '../images/images.utils';
 import { Image } from '../images/models/image.entity';
+import { User } from '../users/user.entity';
+import { Vote } from '../votes/vote.entity';
+import {
+  sortConsensusVotesByType,
+  sortMajorityVotesByType,
+} from '../votes/votes.utils';
+import { ProposalConfig } from './models/proposal-config.entity';
 import { Proposal } from './models/proposal.entity';
 
 const proposalRepository = dataSource.getRepository(Proposal);
@@ -20,6 +27,85 @@ export const getProposals = (
   relations?: string[],
 ) => {
   return proposalRepository.find({ where, relations });
+};
+
+// TODO: Implement: Return eligible voters for a given proposal
+export const getProposalMembers = async () => {
+  return [];
+};
+
+export const isProposalRatifiable = async (proposalId: string) => {
+  const { votes, stage, config } = await getProposal(proposalId, [
+    'config',
+    'votes',
+  ]);
+  const members = await getProposalMembers();
+
+  if (stage !== 'voting') {
+    return false;
+  }
+  if (config.decisionMakingModel === 'consensus') {
+    return hasConsensus(votes, config, members);
+  }
+  if (config.decisionMakingModel === 'consent') {
+    return hasConsent(votes, config);
+  }
+  if (config.decisionMakingModel === 'majority-vote') {
+    return hasMajorityVote(votes, config, members);
+  }
+  return false;
+};
+
+export const hasConsensus = async (
+  votes: Vote[],
+  {
+    ratificationThreshold,
+    reservationsLimit,
+    standAsidesLimit,
+    closingAt,
+  }: ProposalConfig,
+  members: User[],
+) => {
+  if (closingAt && Date.now() < Number(closingAt)) {
+    return false;
+  }
+
+  const { agreements, reservations, standAsides, blocks } =
+    sortConsensusVotesByType(votes);
+
+  return (
+    agreements.length >= members.length * (ratificationThreshold * 0.01) &&
+    reservations.length <= reservationsLimit &&
+    standAsides.length <= standAsidesLimit &&
+    blocks.length === 0
+  );
+};
+
+export const hasConsent = (votes: Vote[], proposalConfig: ProposalConfig) => {
+  const { reservations, standAsides, blocks } = sortConsensusVotesByType(votes);
+  const { reservationsLimit, standAsidesLimit, closingAt } = proposalConfig;
+
+  return (
+    Date.now() >= Number(closingAt) &&
+    reservations.length <= reservationsLimit &&
+    standAsides.length <= standAsidesLimit &&
+    blocks.length === 0
+  );
+};
+
+export const hasMajorityVote = (
+  votes: Vote[],
+  { ratificationThreshold, closingAt }: ProposalConfig,
+  groupMembers: User[],
+) => {
+  if (closingAt && Date.now() < Number(closingAt)) {
+    return false;
+  }
+  const { agreements } = sortMajorityVotesByType(votes);
+
+  return (
+    agreements.length >= groupMembers.length * (ratificationThreshold * 0.01)
+  );
 };
 
 export const createProposal = async (
@@ -55,6 +141,12 @@ export const createProposal = async (
   });
 
   return proposal;
+};
+
+export const ratifyProposal = async (proposalId: string) => {
+  await proposalRepository.update(proposalId, {
+    stage: 'ratified',
+  });
 };
 
 export const deleteProposal = async (proposalId: string) => {
