784d8643da917d87dd37c90fc8e8fb7f9c5998bd
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-03-09 22:30:23 -0400

    feat: add proposal routes

--- src/integrations/integrations.router.ts
+++ src/integrations/integrations.router.ts
@@ -1,3 +1,13 @@
 import express from 'express';
+import {
+  createProposal,
+  deleteProposal,
+  getProposal,
+} from '../proposals/proposals.controller';
 
 export const integrationsRouter = express.Router();
+
+integrationsRouter
+  .get('/proposals', getProposal)
+  .post('/proposals', createProposal)
+  .delete('/proposals/:proposalId', deleteProposal);
