1d5b10ceda66aae7ea57983e2e089d1c8999f66c
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-03-15 19:41:47 -0400

    feat: set authIntegration in integrations router

--- src/integrations/integrations.router.ts
+++ src/integrations/integrations.router.ts
@@ -5,8 +5,11 @@ import {
   getProposal,
 } from '../proposals/proposals.controller';
 import { createVote, deleteVote, updateVote } from '../votes/votes.controller';
+import { authIntegration } from './auth-integration.middleware';
 
 export const integrationsRouter = express.Router();
+
+integrationsRouter.use(authIntegration);
 
 integrationsRouter
   .get('/proposals', getProposal)
