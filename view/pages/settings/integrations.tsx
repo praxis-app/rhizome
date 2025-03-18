27c6de440f8c1e790611700c56ee79c720e5ff2e
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-03-18 19:22:57 -0400

    feat: add integrations form stub

--- view/pages/settings/integrations.tsx
+++ view/pages/settings/integrations.tsx
@@ -1,6 +1,16 @@
+import {
+  Box,
+  Card,
+  CardContent,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  OutlinedInput,
+} from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import TopNav from '../../components/nav/top-nav';
+import PrimaryButton from '../../components/shared/primary-button';
 import { NavigationPaths } from '../../constants/shared.constants';
 
 const Integrations = () => {
@@ -13,6 +23,25 @@ const Integrations = () => {
         header={t('settings.labels.integrations')}
         onBackClick={() => navigate(NavigationPaths.Settings)}
       />
+
+      <Card>
+        <CardContent>
+          <FormGroup sx={{ marginBottom: 1.5 }}>
+            <FormControl>
+              <FormLabel sx={{ fontWeight: 500, paddingBottom: 1 }}>
+                {t('settings.labels.discordClientId')}
+              </FormLabel>
+              <OutlinedInput autoComplete="off" />
+            </FormControl>
+          </FormGroup>
+
+          <Box display="flex" justifyContent="end">
+            <PrimaryButton sx={{ marginTop: 1.5 }} type="submit">
+              {t('settings.actions.connectDiscord')}
+            </PrimaryButton>
+          </Box>
+        </CardContent>
+      </Card>
     </>
   );
 };
