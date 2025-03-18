ea1d5b09e3aa57683215fa741baccdd1c0f81de5
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-03-18 19:08:21 -0400

    feat: add integrations page header

--- view/pages/settings/integrations.tsx
+++ view/pages/settings/integrations.tsx
@@ -1,5 +1,20 @@
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import TopNav from '../../components/nav/top-nav';
+import { NavigationPaths } from '../../constants/shared.constants';
+
 const Integrations = () => {
-  return <>TODO: Add layout and functionality for integrations settings here</>;
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <TopNav
+        header={t('settings.labels.integrations')}
+        onBackClick={() => navigate(NavigationPaths.Settings)}
+      />
+    </>
+  );
 };
 
 export default Integrations;
