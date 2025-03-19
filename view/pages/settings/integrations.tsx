ccf3309092f8bf4609308148b871c52a313979c2
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-03-18 21:37:28 -0400

    feat: update client ID

--- view/pages/settings/integrations.tsx
+++ view/pages/settings/integrations.tsx
@@ -7,13 +7,25 @@ import {
   FormLabel,
   OutlinedInput,
 } from '@mui/material';
+import { useMutation } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import { api } from '../../client/api-client';
 import TopNav from '../../components/nav/top-nav';
 import PrimaryButton from '../../components/shared/primary-button';
 import { NavigationPaths } from '../../constants/shared.constants';
+import { UpdateServerConfigReq } from '../../types/server-config.types';
 
 const Integrations = () => {
+  const { mutate: updateConfig, isPending } = useMutation({
+    mutationFn: api.updateServerConfig,
+  });
+
+  const { register, handleSubmit } = useForm<UpdateServerConfigReq>({
+    mode: 'onChange',
+  });
+
   const { t } = useTranslation();
   const navigate = useNavigate();
 
@@ -26,20 +38,29 @@ const Integrations = () => {
 
       <Card>
         <CardContent>
-          <FormGroup sx={{ marginBottom: 1.5 }}>
-            <FormControl>
-              <FormLabel sx={{ fontWeight: 500, paddingBottom: 1 }}>
-                {t('settings.labels.discordClientId')}
-              </FormLabel>
-              <OutlinedInput autoComplete="off" />
-            </FormControl>
-          </FormGroup>
+          <form onSubmit={handleSubmit((fv) => updateConfig(fv))}>
+            <FormGroup sx={{ marginBottom: 1.5 }}>
+              <FormControl>
+                <FormLabel sx={{ fontWeight: 500, paddingBottom: 1 }}>
+                  {t('settings.labels.discordClientId')}
+                </FormLabel>
+                <OutlinedInput
+                  autoComplete="off"
+                  {...register('botClientId')}
+                />
+              </FormControl>
+            </FormGroup>
 
-          <Box display="flex" justifyContent="end">
-            <PrimaryButton sx={{ marginTop: 1.5 }} type="submit">
-              {t('settings.actions.connectDiscord')}
-            </PrimaryButton>
-          </Box>
+            <Box display="flex" justifyContent="end">
+              <PrimaryButton
+                disabled={isPending}
+                sx={{ marginTop: 1.5 }}
+                type="submit"
+              >
+                {t('settings.actions.connectDiscord')}
+              </PrimaryButton>
+            </Box>
+          </form>
         </CardContent>
       </Card>
     </>
