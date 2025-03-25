32703e752a187e2655862abb1a3d7ff296c6d489
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-03-24 21:52:06 -0400

    feat: add perm checks for server config routes

--- src/server-configs/server-configs.router.ts
+++ src/server-configs/server-configs.router.ts
@@ -1,18 +1,31 @@
 import express from 'express';
 import { authenticate } from '../auth/middleware/authenticate.middleware';
+import { can } from '../roles/middleware/can.middleware';
 import { validateConnectBot } from './middleware/validate-connect-bot.middleware';
-import {
-  connectBot,
-  disconnectBot,
-  getServerConfig,
-  updateServerConfig,
-} from './server-configs.controller';
+import * as serverConfigsController from './server-configs.controller';
 
 export const serverConfigsRouter = express.Router();
 
 serverConfigsRouter
   .use(authenticate)
-  .get('/', getServerConfig)
-  .put('/', updateServerConfig)
-  .post('/connect-bot', validateConnectBot, connectBot)
-  .delete('/disconnect-bot', disconnectBot);
+  .get(
+    '/',
+    can('read', 'ServerConfig'),
+    serverConfigsController.getServerConfig,
+  )
+  .put(
+    '/',
+    can('update', 'ServerConfig'),
+    serverConfigsController.updateServerConfig,
+  )
+  .post(
+    '/connect-bot',
+    can('manage', 'ServerConfig'),
+    validateConnectBot,
+    serverConfigsController.connectBot,
+  )
+  .delete(
+    '/disconnect-bot',
+    can('manage', 'ServerConfig'),
+    serverConfigsController.disconnectBot,
+  );
