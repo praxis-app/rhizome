a8c368a69c80ca28a499b081f0f63ea259cf5d2a
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-03-24 22:33:12 -0400

    feat: add perm checks for integrations page

--- view/pages/settings/integrations.tsx
+++ view/pages/settings/integrations.tsx
@@ -4,17 +4,36 @@ import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { api } from '../../client/api-client';
 import TopNav from '../../components/nav/top-nav';
+import PermissionDenied from '../../components/roles/permission-denied';
 import ConnectDiscordForm from '../../components/settings/connect-discord-form';
+import ProgressBar from '../../components/shared/progress-bar';
 import { NavigationPaths } from '../../constants/shared.constants';
+import { useAbility } from '../../hooks/role.hooks';
 
 const Integrations = () => {
-  const { data: serverConfigData } = useQuery({
+  const { data: serverConfigData, isLoading } = useQuery({
     queryKey: ['serverConfig'],
     queryFn: () => api.getServerConfig(),
   });
 
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const ability = useAbility();
+
+  if (!ability.can('manage', 'ServerConfig')) {
+    return (
+      <PermissionDenied
+        topNavProps={{
+          header: t('roles.headers.serverRoles'),
+          onBackClick: () => navigate(NavigationPaths.Settings),
+        }}
+      />
+    );
+  }
+
+  if (isLoading) {
+    return <ProgressBar />;
+  }
 
   return (
     <>
